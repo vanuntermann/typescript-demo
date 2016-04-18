c4251b3dc4b08efb28328d1d454d843564dc1ee4
Alexander Noriega <lambdatoast@gmail.com>
2016-04-18 00:47:53 -0300

    Use Error instead of string for form validation example.

--- src/examples/safe-construction-type-driven-style.ts
+++ src/examples/safe-construction-type-driven-style.ts
@@ -13,10 +13,9 @@ module SafeConstruction {
       return new FormData(data, new Unvalidated('Initial form'));
     }
 
-    export function validate(x: FormData<Unvalidated>): FormData<Validated> | string {
+    export function validate(x: FormData<Unvalidated>): FormData<Validated> | Error {
       if (x.getData() == '') {
-        //throw new Error('Empty string');
-        return 'Empty string is not a valid code';
+        return new Error('Empty string is not a valid code');
       } else {
         return new FormData(x.getData(), new Validated(true));
       }
@@ -34,11 +33,13 @@ module SafeConstruction {
     let av = validate(a);
     export let r0 : string;
 
-    if (typeof av === 'string') {
-      r0 = av;
+    // We *have* to branch now
+    // i.e. type system *statically* forces us to write a *runtime* check.
+    if (av instanceof Error) {
+      r0 = av.message;
     } else {
-      r0 = process(av); // process(a) would be a type error: unvalidated form
-                        // process(av) outside this `else` clause would also be a type error: non-forked `validated | error` union type
+      r0 = process(av); // `process(a)` would be a type error: unvalidated form
+                        // `process(av)` outside this `else` would also be a type error: non-forked `validated | error` union type
     }
   }
 }
